a251f430566983250bc00bbb49e027822b1ad72e
jacoblim2023 <128806551+jacoblim2023@users.noreply.github.com>
2023-05-05 18:45:13 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("my first console project");
+var first_name = "Jacob";
+var last_name = "Lim";
+var full_name = first_name.concat(last_name)
+console.log(full_name)
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log(add)
+var sub = a-b;
+console.log(sub)
+var mul = a*b;
+console.log(mul)
